81759758763e768dcc9109eb625ccc01c8795c5d
Sergiy Labay <oodeesay@gmail.com>
2025-02-12 18:58:50 +0200

    DB backup added

--- WeatherForecast/WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
+++ WeatherForecast/WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001ac02d6e7b665ca0136b71499aa11de6bef05a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
